83c0d94de20a2693696d2a362309220790e76c0b
beefykenny <hoftkenny@gmail.com>
2021-08-20 01:13:51 -0300

    made singularPost component so I can put rating in state for each post

--- React_Frontend/blogapi/src/components/content/posts.jsx
+++ React_Frontend/blogapi/src/components/content/posts.jsx
@@ -5,11 +5,10 @@ import ReactPaginate from "react-paginate";
 import UserContext from "./../../context/userContext";
 import ReactTooltip from "react-tooltip";
 import axiosInstance from "./../../baseAxios";
+import SingularPost from "./singularPost";
 
 export default class App extends Component {
   state = { offset: 0, data: [], perPage: 6, currentPage: 0 };
-
-  static contextType = DarkContext;
 
   componentDidMount() {
     if (this.props.posts) {
@@ -42,7 +41,7 @@ export default class App extends Component {
     console.log("reacted to " + postId + " with " + vote);
   };
 
-  getFormattedRating = (rating) => {
+  getRatingSuffix = (rating) => {
     if (rating > 999999999) {
       const num = (rating / 1000000000).toFixed(1);
       return `${num}B`;
@@ -58,26 +57,6 @@ export default class App extends Component {
 
     return rating.toString();
   };
-
-  likeDislike(post, action) {
-    if (action === "like") {
-      const postRate = parseInt(post.rating);
-      console.log(postRate, post.slug);
-
-      const restPost = {
-        title: post.title,
-        slug: post.slug,
-        content: post.content,
-        author: post.author,
-      };
-
-      console.log(restPost);
-      axiosInstance
-        .put("update-post/" + post.slug, { ...restPost, rating: postRate + 1 })
-        .then((res) => console.log(res.data))
-        .catch((res) => console.log(res.response));
-    }
-  }
 
   receivedData() {
     const data = this.props.posts;
@@ -101,118 +80,21 @@ export default class App extends Component {
 
       const dataType = this.context.darkMode ? "light" : "dark";
 
-      const toPass = {
-        title: title,
-        content: content,
-        slug: slug,
-        author: author,
-        image: image,
-        rating: rating,
-        postDate: postDate,
-        day: day,
-        dataType: dataType,
-        post: post,
-      };
-
       return (
-        <div
-          className={
-            this.context.darkMode
-              ? "blog-post text-light border bg-dark"
-              : "blog-post border"
-          }
-          key={post.id}
-        >
-          <ReactTooltip />
-          <div className="blog-post-img border">
-            <img src={image} alt={"Image for: " + title} />
-          </div>
-          <div className="blog-post-info">
-            <div className="blog-post-date">
-              <span>{day}</span>
-              <span>{postDate}</span>
-            </div>
-            <div className="blog-post-author">
-              <UserContext.Consumer>
-                {(userContext) => {
-                  return (
-                    <React.Fragment>
-                      by <span>{userContext.getAuthor(author)}</span>
-                    </React.Fragment>
-                  );
-                }}
-              </UserContext.Consumer>
-            </div>
-
-            <h1 className="blog-post-title">{title}</h1>
-            <p className="blog-post-text">
-              {content.length >= 500
-                ? content.substring(0, 500) + "..."
-                : content}
-            </p>
-            <Link to={"posts/" + slug} className="blog-post-cta">
-              Read More
-            </Link>
-          </div>
-          <div className="blog-post-options">
-            <div className="react">
-              <div className="rating badge badge-primary">
-                <span
-                  data-tip={
-                    "rating: " +
-                    rating.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
-                  }
-                  data-type={dataType}
-                >
-                  {rating ? this.getFormattedRating(rating) : 0}
-                </span>
-              </div>
-              <div className="upvote">
-                <button
-                  className="btn btn-success"
-                  data-tip="like"
-                  data-type={dataType}
-                  onClick={() => this.likeDislike(post, "like")}
-                >
-                  <i className="fas fa-thumbs-up"></i>
-                </button>
-              </div>
-              <div className="downvote">
-                <button
-                  className="btn btn-danger"
-                  data-tip="dislike"
-                  data-type={dataType}
-                >
-                  <i className="fas fa-thumbs-down"></i>
-                </button>
-              </div>
-            </div>
-            <div className="other-react">
-              <div className="bookmark">
-                <button
-                  className="btn btn-info"
-                  data-tip="add to bookmarks"
-                  data-type={dataType}
-                >
-                  <i className="far fa-bookmark"></i>
-                  {/* <i className="fas fa-bookmark"></i> */}
-                </button>
-              </div>
-              <div
-                className="comment"
-                data-tip="comment on this post"
-                data-type={dataType}
-              >
-                <button className="btn btn-dark">
-                  <i
-                    style={{ fontSize: ".8rem" }}
-                    className="far fa-comment-alt"
-                  ></i>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <SingularPost
+          title={title}
+          content={content}
+          slug={slug}
+          author={author}
+          image={image}
+          rating={rating}
+          postDate={postDate}
+          day={day}
+          dataType={dataType}
+          post={post}
+          getRatingSuffix={this.getRatingSuffix}
+          dataType={dataType}
+        />
       );
     });
 
---
+++ React_Frontend/blogapi/src/components/content/singularPost.jsx
@@ -0,0 +1,175 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import UserContext from "./../../context/userContext";
+import ReactTooltip from "react-tooltip";
+import axiosInstance from "./../../baseAxios";
+import DarkContext from "./../../context/darkMode";
+
+export default class SingularPost extends Component {
+  state = { rating: 0 };
+  static contextType = DarkContext;
+
+  componentDidMount() {
+    axiosInstance.get("posts/" + this.props.post.slug).then((res) => {
+      console.log(res.data.title + " has rating: " + res.data.rating);
+      this.setState({ rating: res.data.rating });
+    });
+  }
+
+  likeDislike(post, value) {
+    const { rating } = this.state;
+
+    console.log("rating before", rating);
+
+    this.setState({ rating: rating + value });
+
+    console.log("rating after", rating);
+
+    const restPost = {
+      title: post.title,
+      slug: post.slug,
+      content: post.content,
+      author: post.author,
+    };
+
+    axiosInstance
+      .put("update-post/" + post.slug, {
+        ...restPost,
+        rating: rating + value,
+      })
+      .then((res) => console.log(res.data));
+  }
+
+  getRatingColor = (rate) => {
+    const prefix = "rating badge ";
+
+    switch (rate) {
+      case rate > 999:
+        return prefix + "rating1000";
+
+      case 0:
+        return prefix;
+
+      default:
+        return "rating badge badge-primary";
+    }
+  };
+  render() {
+    const {
+      title,
+      content,
+      slug,
+      author,
+      image,
+      postDate,
+      day,
+      dataType,
+      post,
+
+      getRatingSuffix,
+    } = this.props;
+
+    const rating = this.state.rating;
+    return (
+      <div
+        className={
+          this.context.darkMode
+            ? "blog-post text-light border bg-dark"
+            : "blog-post border"
+        }
+        key={post.id}
+      >
+        <ReactTooltip />
+        <div className="blog-post-img border">
+          <img src={image} alt={"Image for: " + title} />
+        </div>
+        <div className="blog-post-info">
+          <div className="blog-post-date">
+            <span>{day}</span>
+            <span>{postDate}</span>
+          </div>
+          <div className="blog-post-author">
+            <UserContext.Consumer>
+              {(userContext) => {
+                return (
+                  <React.Fragment>
+                    by <span>{userContext.getAuthor(author)}</span>
+                  </React.Fragment>
+                );
+              }}
+            </UserContext.Consumer>
+          </div>
+
+          <h1 className="blog-post-title">{title}</h1>
+          <p className="blog-post-text">
+            {content.length >= 500
+              ? content.substring(0, 500) + "..."
+              : content}
+          </p>
+          <Link to={"posts/" + slug} className="blog-post-cta">
+            Read More
+          </Link>
+        </div>
+        <div className="blog-post-options">
+          <div className="react">
+            <div className={this.getRatingColor(rating)}>
+              <span
+                data-tip={
+                  "rating: " +
+                  rating.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                }
+                data-type={dataType}
+              >
+                {getRatingSuffix(rating)}
+              </span>
+            </div>
+            <div className="upvote">
+              <button
+                className="btn btn-success"
+                data-tip="like"
+                data-type={dataType}
+                onClick={() => this.likeDislike(post, 1)}
+              >
+                <i className="fas fa-thumbs-up"></i>
+              </button>
+            </div>
+            <div className="downvote">
+              <button
+                className="btn btn-danger"
+                data-tip="dislike"
+                data-type={dataType}
+                onClick={() => this.likeDislike(post, -1)}
+              >
+                <i className="fas fa-thumbs-down"></i>
+              </button>
+            </div>
+          </div>
+          <div className="other-react">
+            <div className="bookmark">
+              <button
+                className="btn btn-info"
+                data-tip="add to bookmarks"
+                data-type={dataType}
+              >
+                <i className="far fa-bookmark"></i>
+                {/* <i className="fas fa-bookmark"></i> */}
+              </button>
+            </div>
+            <div
+              className="comment"
+              data-tip="comment on this post"
+              data-type={dataType}
+            >
+              <button className="btn btn-dark">
+                <i
+                  style={{ fontSize: ".8rem" }}
+                  className="far fa-comment-alt"
+                ></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
