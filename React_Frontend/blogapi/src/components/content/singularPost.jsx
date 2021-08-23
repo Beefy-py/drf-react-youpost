12411d6b7d534f593ada193a5c8fc1b8c9ef54fa
beefykenny <hoftkenny@gmail.com>
2021-08-23 15:03:36 -0300

    added add to bookmarks function

--- React_Frontend/blogapi/src/components/content/singularPost.jsx
+++ React_Frontend/blogapi/src/components/content/singularPost.jsx
@@ -13,6 +13,10 @@ export default class SingularPost extends Component {
       atob(localStorage.getItem("access_token").split(".")[1])
     ).user_id,
     currentUser: {},
+    likeClass: "btn btn-outline-dark",
+    dislikeClass: "btn btn-outline-dark",
+    bookmarkClass: "btn btn-outline-dark",
+    bookmarkDataTip: "add to bookmarks",
   };
 
   static contextType = DarkContext;
@@ -26,13 +30,31 @@ export default class SingularPost extends Component {
       .get("posts/" + this.props.post.slug)
       .then((res) => this.setState({ rating: res.data.rating }));
 
-    axiosInstance
-      .get("user/list/" + this.state.currentUserId)
-      .then((res) => this.setState({ currentUser: res.data }));
+    axiosInstance.get("user/list/" + this.state.currentUserId).then((res) => {
+      this.setState({ currentUser: res.data });
+
+      const { post } = this.props;
+
+      if (res.data.liked.includes(post.id)) {
+        this.setState({ likeClass: "btn btn-success disabled" });
+        this.setState({ dislikeClass: "btn btn-outline-dark disabled" });
+      }
+
+      if (res.data.disliked.includes(post.id)) {
+        // console.log(`${res.data.username} has already disliked  ${post.title}`);
+        this.setState({ dislikeClass: "btn btn-danger disabled" });
+        this.setState({ likeClass: "btn btn-outline-dark disabled" });
+      }
+
+      if (res.data.bookmarked.includes(post.id)) {
+        this.setState({ bookmarkClass: "btn btn-info" });
+        this.setState({ bookmarkDataTip: "remove from bookmarks" });
+      }
+    });
   }
 
   changeRating(post, value) {
-    const { rating, currentUser } = this.state;
+    const { rating, currentUser, currentUserId } = this.state;
 
     const restPost = {
       title: post.title,
@@ -42,11 +64,35 @@ export default class SingularPost extends Component {
       published: post.published,
     };
 
-    console.log(currentUser.liked, post.id);
+    let likedPosts = currentUser.liked;
+    let dislikedPosts = currentUser.disliked;
+
+    if (value === 1) {
+      likedPosts.push(post.id);
+      console.log(
+        currentUser.username + " liked " + post.title + " with id " + post.id
+      );
+    } else {
+      dislikedPosts.push(post.id);
+      console.log(
+        currentUser.username + " disliked " + post.title + " with id " + post.id
+      );
+    }
+
+    currentUser.liked = likedPosts;
+    currentUser.dislike = dislikedPosts;
 
     if (currentUser.liked.includes(post.id)) {
-      console.log("has already liuked");
+      this.setState({ likeClass: "btn btn-success disabled" });
+      this.setState({ dislikeClass: "btn btn-outline-dark disabled" });
     }
+
+    if (currentUser.disliked.includes(post.id)) {
+      this.setState({ dislikeClass: "btn btn-danger disabled" });
+      this.setState({ likeClass: "btn btn-outline-dark disabled" });
+    }
+
+    axiosInstance.put("user/list/" + currentUserId, currentUser);
 
     this.setState({ rating: rating + value });
 
@@ -55,6 +101,33 @@ export default class SingularPost extends Component {
       rating: rating + value,
     });
   }
+
+  addBookmark = (post) => {
+    const { currentUser, currentUserId } = this.state;
+    let bookmarked;
+    bookmarked = currentUser.bookmarked;
+    console.log("\n before", currentUser.bookmarked);
+
+    if (currentUser.bookmarked.includes(post.id)) {
+      bookmarked.splice(bookmarked.indexOf(post.id), 1);
+      this.setState({ bookmarkClass: "btn btn-outline-dark" });
+      this.setState({ bookmarkDataTip: "add to bookmarks" });
+    } else {
+      bookmarked.push(post.id);
+      this.setState({ bookmarkClass: "btn btn-info" });
+      this.setState({ bookmarkDataTip: "remove from bookmarks" });
+    }
+
+    currentUser.bookmarked = bookmarked;
+    console.log("\n after", currentUser.bookmarked);
+
+    axiosInstance.put("user/list/" + currentUserId, {
+      ...currentUser,
+      bookmarked: bookmarked,
+    });
+
+    window.location.replace("/dashboard");
+  };
 
   getRatingColor = (rate) => {
     const prefix = "rate badge ";
@@ -87,6 +160,9 @@ export default class SingularPost extends Component {
 
       getRatingSuffix,
     } = this.props;
+
+    const { likeClass, dislikeClass, bookmarkClass, bookmarkDataTip } =
+      this.state;
 
     const rating = this.state.rating;
     const currentUser = this.state.users.filter(
@@ -154,31 +230,32 @@ export default class SingularPost extends Component {
                   <div className="react">
                     <div className="upvote">
                       <button
-                        className="btn btn-success"
+                        className={likeClass}
                         data-tip="like"
                         data-type={dataType}
                         onClick={() => this.changeRating(post, 1)}
                       >
-                        <i className="fas fa-thumbs-up"></i>
+                        <i className="far fa-thumbs-up"></i>
                       </button>
                     </div>
                     <div className="downvote">
                       <button
-                        className="btn btn-danger"
+                        className={dislikeClass}
                         data-tip="dislike"
                         data-type={dataType}
                         onClick={() => this.changeRating(post, -1)}
                       >
-                        <i className="fas fa-thumbs-down"></i>
+                        <i className="far fa-thumbs-down"></i>
                       </button>
                     </div>
                   </div>
                   <div className="other-react">
                     <div className="bookmark">
                       <button
-                        className="btn btn-info"
-                        data-tip="add to bookmarks"
+                        className={bookmarkClass}
+                        data-tip={bookmarkDataTip}
                         data-type={dataType}
+                        onClick={() => this.addBookmark(post)}
                       >
                         <i className="far fa-bookmark"></i>
                         {/* <i className="fas fa-bookmark"></i> */}
